2e6a1751cf03a0fc24260383283baae36d37169f
zhangweipeng <zhangweipengphp@163.com>
2018-08-28 01:19:16 +0800

    commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -53,12 +53,7 @@
         </div>
     </div>
     @endforeach
-    {{--<div class="row">--}}
-        {{--<div class="col-md-4"></div>--}}
-        {{--<div class="col-md-4">--}}
-            {{--<code><?php dd($info) ?></code>--}}
-        {{--</div>--}}
-    {{--</div>--}}
+
 </div>
 
 <!-- Optional JavaScript -->
