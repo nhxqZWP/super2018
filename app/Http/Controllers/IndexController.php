6dbf955dd4e7489cc12b7b86af1004e947e377c5
zhangweipeng <zhangweipengphp@163.com>
2018-09-02 21:20:20 +0800

    commit

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -15,7 +15,7 @@ class IndexController extends Controller
      {
           $api = new Binance(PlatformService::BinanceGetKey(), PlatformService::BinanceGetSecret());
           $history = $api->history('EOSUSDT', 100);
-
+dd($history);
           dd(date('Y-m-d H:i:s', ceil(1534601523440/1000)));
 
           $key = StrategyService::THREE_DOWN_BTCUSDT;
